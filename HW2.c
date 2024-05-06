10473395295ded6cac4da47e27314922902ce5fb
Chzhang09 <u11202209@o365.ttu.edu.tw>
2024-05-06 16:54:27 +0800

    push

--- HW2.c
+++ HW2.c
@@ -2,7 +2,25 @@
 #include<stdlib.h>
 
 int main(){
-	
+	int s;/*宣告整數變數*/ 
+	char a=92,b=47,w=95,z=124,r=45,e='e',c;/*宣告字元變數*/ 
+	/*Tip 1*/
+	printf(" -------------------------------------------------------------- \n");
+	printf("%c                                                %c             %c\n",z,w,z);
+	printf("%c  %c                  %c%c                 %c_%c    %c_%c            %c\n",z,a,b,a,b,b,z,z,z);
+	printf("%c   %c                %c_%c%c               %c_%c     %c_%c            %c\n",z,a,b,b,a,b,b,z,z,z);
+	printf("%c    %c              %c_%c  %c             %c_%c      %c_%c            %c\n",z,a,b,b,a,b,b,z,z,z);
+	printf("%c     %c            %c_%c    %c           %c_%c       %c_%c            %c\n",z,a,b,b,a,b,b,z,z,z);
+	printf("%c      %c          %c_%c      %c         %c_%c        %c_%c            %c\n",z,a,b,b,a,b,b,z,z,z);
+	printf("%c       %c        %c_%c        %c       %c_%c %c%c%c%c%c%c%c %c_%c   %c%c%c%c%c    %c\n",z,a,b,b,a,b,b,w,w,w,w,w,w,w,z,z,w,w,w,w,w,z);
+	printf("%c        %c      %c_%c          %c     %c_%c %c        %c_%c  %c     %c   %c\n",z,a,b,b,a,b,b,z,z,z,z,z,z);
+	printf("%c         %c    %c_%c            %c   %c_%c  %c        %c_%c  %c     %c   %c\n",z,a,b,b,a,b,b,z,z,z,z,z);
+	printf("%c          %c  %c_%c              %c %c_%c   %c        %c_%c  %c     %c   %c\n",z,a,b,b,a,b,b,z,z,z,z,z);
+	printf("%c           %c%c_%c                %c_%c  %c %c%c%c%c%c%c%c%c %c%c%c  %c%c%c%c%c%c%c   %c\n",z,a,b,b,a,b,e,z,w,w,w,w,w,w,w,z,w,z,z,w,w,w,w,w,z,z);
+	printf("%c  %c%c%c%c%c                                                       %c\n",z,69,49,66,48,57,z);
+	printf(" -------------------------------------------------------------- \n");/*以上為個人風格畫面*/ 
+	system("pause");/*按鍵操控進入步驟2*/
+	system("cls");/*將已經輸出於螢幕的文字清除，並且回到主程式第二行*/
 	
 	
 	
